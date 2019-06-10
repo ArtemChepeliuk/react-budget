cc248f23143ceb1a92a891e6e7e320e9e5fbee37
artemchepeliuk <4epeliuk@gmail.com>
2019-06-10 15:00:34 +0300

    first commit

---
+++ precache-manifest.5d938e596a3df8ead2e53e3bc533297d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80be7eafa66e2078d70be469096b715f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3491821bdc28ee2c595a",
+    "url": "/static/css/main.e20d87c3.chunk.css"
+  },
+  {
+    "revision": "2a5c13009e2205c19cf4",
+    "url": "/static/js/2.5a0e8634.chunk.js"
+  },
+  {
+    "revision": "3491821bdc28ee2c595a",
+    "url": "/static/js/main.5f4967aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
